8a61e12572924611b7df7aac323ec6cbcc45b288
mbcdiscord <bodepudiyethinsai@gmail.com>
2023-06-08 11:17:55 +0530

    q

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ app.get("/", (req, res) => {
         res.send("Hello World...");
     }
 });
+//https://blog.pagesd.info/2019/10/08/crud-with-express-sqlite-10-steps/#:~:text=in%20this%20tutorial.-,4.%20Add%20EJS%20views,-Since%20the%20purpose
