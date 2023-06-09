2a2cf4344c9be77f889855f83d96a9178dad53e7
mbcdiscord <bodepudiyethinsai@gmail.com>
2023-06-09 09:24:44 +0530

    q

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
-~~~
-    app.listen(3000, () => {
-        {
-            console.log("Server started (http://localhost:3000/) !");
 
-        }
-    });
+app.listen(3000, () => {
+    {
+        console.log("Server started (http://localhost:3000/) !");
+
+    }
+});
 
 
 app.get("/", (req, res) => {
@@ -15,3 +15,6 @@ app.get("/", (req, res) => {
         res.send("Hello World...");
     }
 });
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
