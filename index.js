07adfc6c74fac26c7c07a8e024f947d1245e6dfb
mbcdiscord <bodepudiyethinsai@gmail.com>
2023-06-09 09:25:28 +0530

    1

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get("/", (req, res) => {
         res.send("Hello World...");
     }
 });
+
 app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
