47929ab04fdb7d441f69a2d3a151e1a735f2c16e
mbcdiscord <bodepudiyethinsai@gmail.com>
2023-06-09 09:19:54 +0530

    1

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 ~~~
-app.listen(3000, () => {
-    {
-        console.log("Server started (http://localhost:3000/) !");
+    app.listen(3000, () => {
+        {
+            console.log("Server started (http://localhost:3000/) !");
 
-    }
-});
+        }
+    });
 
 
 app.get("/", (req, res) => {
@@ -15,4 +15,3 @@ app.get("/", (req, res) => {
         res.send("Hello World...");
     }
 });
-//https://blog.pagesd.info/2019/10/08/crud-with-express-sqlite-10-steps/#:~:text=in%20this%20tutorial.-,4.%20Add%20EJS%20views,-Since%20the%20purpose
